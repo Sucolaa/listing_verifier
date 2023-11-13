6325982556c47a064a3efe3541272a0d950e3246
SuXu <67124762+Sucolaa@users.noreply.github.com>
2023-11-13 16:34:06 +0800

    home try

--- server.R
+++ server.R
@@ -1,3 +1,7 @@
+library(shiny)
 source("share_loads.R")
-walk(list.files("ui_scripts", full.names = TRUE), ~ source(.x))
+# walk(list.files("ui_scripts", full.names = TRUE), ~ source(.x))
 
+function(input, output, session) {
+  
+}
